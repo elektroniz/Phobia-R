9d34e1af2c1bc4e261efc4da7c5e3d209469ac09
Korvux <35342324+KorvuxNeb@users.noreply.github.com>
2018-05-11 22:22:04 +0200

    Add files via upload

---
+++ Phobia-R/MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Phobia_R
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.MovablePanel = new System.Windows.Forms.Panel();
+            this.UnderBox = new System.Windows.Forms.PictureBox();
+            this.CloseBox = new System.Windows.Forms.PictureBox();
+            this.labelManualMode = new System.Windows.Forms.Label();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.UnderWindowPanel = new System.Windows.Forms.Panel();
+            this.PhobiarBox = new System.Windows.Forms.PictureBox();
+            this.RightArrowBox = new System.Windows.Forms.PictureBox();
+            this.MovablePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UnderBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PhobiarBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightArrowBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.inputBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputBox.Font = new System.Drawing.Font("Anonymice Powerline", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.inputBox.Location = new System.Drawing.Point(41, 190);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(454, 346);
+            this.inputBox.TabIndex = 1;
+            // 
+            // outputBox
+            // 
+            this.outputBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.outputBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.outputBox.Font = new System.Drawing.Font("Anonymice Powerline", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(91)))), ((int)(((byte)(102)))));
+            this.outputBox.Location = new System.Drawing.Point(658, 190);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(454, 346);
+            this.outputBox.TabIndex = 2;
+            // 
+            // MovablePanel
+            // 
+            this.MovablePanel.Controls.Add(this.UnderBox);
+            this.MovablePanel.Controls.Add(this.CloseBox);
+            this.MovablePanel.Location = new System.Drawing.Point(-5, -4);
+            this.MovablePanel.Name = "MovablePanel";
+            this.MovablePanel.Size = new System.Drawing.Size(1158, 43);
+            this.MovablePanel.TabIndex = 3;
+            this.MovablePanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MovablePanel_MouseDown);
+            // 
+            // UnderBox
+            // 
+            this.UnderBox.Image = global::Phobia_R.Properties.Resources.horizontal_line_remove_buttonwhite;
+            this.UnderBox.Location = new System.Drawing.Point(1068, 16);
+            this.UnderBox.Name = "UnderBox";
+            this.UnderBox.Size = new System.Drawing.Size(33, 17);
+            this.UnderBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.UnderBox.TabIndex = 6;
+            this.UnderBox.TabStop = false;
+            this.UnderBox.Click += new System.EventHandler(this.UnderBox_Click);
+            // 
+            // CloseBox
+            // 
+            this.CloseBox.Image = global::Phobia_R.Properties.Resources.close_buttonwhite;
+            this.CloseBox.Location = new System.Drawing.Point(1107, 16);
+            this.CloseBox.Name = "CloseBox";
+            this.CloseBox.Size = new System.Drawing.Size(33, 17);
+            this.CloseBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CloseBox.TabIndex = 5;
+            this.CloseBox.TabStop = false;
+            this.CloseBox.Click += new System.EventHandler(this.CloseBox_Click);
+            // 
+            // labelManualMode
+            // 
+            this.labelManualMode.AutoSize = true;
+            this.labelManualMode.Font = new System.Drawing.Font("Anonymice Powerline", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelManualMode.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(130)))), ((int)(((byte)(118)))));
+            this.labelManualMode.Location = new System.Drawing.Point(35, 144);
+            this.labelManualMode.Name = "labelManualMode";
+            this.labelManualMode.Size = new System.Drawing.Size(203, 32);
+            this.labelManualMode.TabIndex = 6;
+            this.labelManualMode.Text = "Manual Mode";
+            // 
+            // urlBox
+            // 
+            this.urlBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.urlBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.urlBox.Font = new System.Drawing.Font("Anonymice Powerline", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.urlBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.urlBox.Location = new System.Drawing.Point(41, 557);
+            this.urlBox.Multiline = true;
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(454, 21);
+            this.urlBox.TabIndex = 7;
+            // 
+            // UnderWindowPanel
+            // 
+            this.UnderWindowPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(64)))), ((int)(((byte)(51)))));
+            this.UnderWindowPanel.ForeColor = System.Drawing.Color.Coral;
+            this.UnderWindowPanel.Location = new System.Drawing.Point(-5, 634);
+            this.UnderWindowPanel.Name = "UnderWindowPanel";
+            this.UnderWindowPanel.Size = new System.Drawing.Size(1158, 35);
+            this.UnderWindowPanel.TabIndex = 8;
+            // 
+            // PhobiarBox
+            // 
+            this.PhobiarBox.Image = global::Phobia_R.Properties.Resources.logo;
+            this.PhobiarBox.Location = new System.Drawing.Point(38, 12);
+            this.PhobiarBox.Name = "PhobiarBox";
+            this.PhobiarBox.Size = new System.Drawing.Size(230, 43);
+            this.PhobiarBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PhobiarBox.TabIndex = 5;
+            this.PhobiarBox.TabStop = false;
+            this.PhobiarBox.Click += new System.EventHandler(this.PhobiarBox_Click);
+            // 
+            // RightArrowBox
+            // 
+            this.RightArrowBox.Image = global::Phobia_R.Properties.Resources.keyboard_right_arrow_button;
+            this.RightArrowBox.Location = new System.Drawing.Point(501, 329);
+            this.RightArrowBox.Name = "RightArrowBox";
+            this.RightArrowBox.Size = new System.Drawing.Size(151, 76);
+            this.RightArrowBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.RightArrowBox.TabIndex = 4;
+            this.RightArrowBox.TabStop = false;
+            this.RightArrowBox.Click += new System.EventHandler(this.RightArrowBox_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(1147, 668);
+            this.Controls.Add(this.UnderWindowPanel);
+            this.Controls.Add(this.urlBox);
+            this.Controls.Add(this.labelManualMode);
+            this.Controls.Add(this.PhobiarBox);
+            this.Controls.Add(this.RightArrowBox);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.MovablePanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MovablePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.UnderBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PhobiarBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightArrowBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Panel MovablePanel;
+        private System.Windows.Forms.PictureBox RightArrowBox;
+        private System.Windows.Forms.PictureBox UnderBox;
+        private System.Windows.Forms.PictureBox CloseBox;
+        private System.Windows.Forms.PictureBox PhobiarBox;
+        private System.Windows.Forms.Label labelManualMode;
+        private System.Windows.Forms.TextBox urlBox;
+        private System.Windows.Forms.Panel UnderWindowPanel;
+    }
+}
+
---
+++ Phobia-R/MainForm.cs
@@ -0,0 +1,148 @@
+﻿/*
+██╗  ██╗ ██████╗ ██████╗ ██╗   ██╗██╗   ██╗██╗  ██╗
+██║ ██╔╝██╔═══██╗██╔══██╗██║   ██║██║   ██║╚██╗██╔╝
+█████╔╝ ██║   ██║██████╔╝██║   ██║██║   ██║ ╚███╔╝ 
+██╔═██╗ ██║   ██║██╔══██╗╚██╗ ██╔╝██║   ██║ ██╔██╗ 
+██║  ██╗╚██████╔╝██║  ██║ ╚████╔╝ ╚██████╔╝██╔╝ ██╗
+╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝  ╚═══╝   ╚═════╝ ╚═╝  ╚═╝
+###################################################
+Software Version: 0.1
+Author: Korvux
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phobia_R
+{
+    public partial class MainForm : Form
+    {
+        // This method is automatically created and managed by Windows Forms designer and it defines everything you see on the form.
+        public MainForm() => InitializeComponent();
+
+
+        // -------------------------- Temporany location --------------------------
+        // Explaination: https://www.codeproject.com/Articles/11114/Move-window-form-without-Titlebar-in-C
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        // --------------------------         End        --------------------------
+
+        // Window functionality ---------------------------------------- Start ----
+        private void MovablePanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void CloseBox_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void UnderBox_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void PhobiarBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Work In progress");
+        }
+
+        private void RightArrowBox_Click(object sender, EventArgs e)
+        {
+            // In this code example, use a hard-coded
+            // IP address and message.
+            string serverIP = urlBox.Text;
+            string message = inputBox.Text;
+            if (message == "" || serverIP == "")
+            {
+                MessageBox.Show("Check inputs!");
+                return;
+            }
+            outputBox.Text = ConnectAndSend(serverIP, message);
+        }
+
+        // Window functionality ----------------------------------------  End  ----
+
+        // Networking Function  ---------------------------------------- Start ----
+        static string ConnectAndSend(string serverIP, string message)
+        {
+            // Socket Creation.
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string body = "Error with calculation";
+
+            // Variable Handling
+            string GETrequest = message + "\r\n\r\n";
+
+            // Checking errors phase
+
+            try { socket.Connect(serverIP, 80); } catch (SocketException) { return "No such host is known, check url!"; }
+            try { socket.Send(Encoding.ASCII.GetBytes(GETrequest)); } catch (SocketException e) { return e.ToString(); }
+
+            bool f = true; // Just so we know we are still reading
+            string headerString = ""; // To store header information
+            int contentLength = 0; // The body length
+            byte[] bodyBuff = new byte[0]; // To later hold the body content
+
+            while (f)
+            {
+                // Read the header byte by byte, until \r\n\r\n
+                byte[] buffer = new byte[1];
+                socket.Receive(buffer, 0, 1, 0);
+
+                try { headerString += Encoding.ASCII.GetString(buffer); } catch (ArgumentNullException e) { return e.ToString(); }
+
+                if (headerString.Contains("\r\n\r\n"))
+                {
+                    // header is received, parsing content length
+                    // I use regular expressions, but any other method you can think of is ok
+                    Regex reg = new Regex("\\\r\nContent-Length: (.*?)\\\r\n");
+                    Match m = reg.Match(headerString);
+
+                    try
+                    {
+                        contentLength = int.Parse(m.Groups[1].ToString());
+                    }
+                    catch (FormatException e)
+                    {
+                        body = e.ToString();
+                        break;
+                    }
+
+                    f = false;  // Stop iteration while used to read the header byte by byte.
+                    // Read the body
+                    bodyBuff = new byte[contentLength];
+                    socket.Receive(bodyBuff, 0, contentLength, 0);
+                }
+            }
+
+            body = Encoding.ASCII.GetString(bodyBuff);
+
+            if (body == "") body = headerString;
+
+            socket.Close();
+
+            return body;
+        }
+    }
+}
